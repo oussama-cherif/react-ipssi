093890088c1f1ea834cfbff615cd66900b612a0f
unknown <cherifoussama1996@gmail.com>
2022-06-30 12:23:23 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import NotFound404 from "./Pages/NotFound404";
+import HeadBodyGrid from "./components/HeadBodyGrid";
+
+const Home = React.lazy(() => import("./Pages/Home"));
+const Projet = React.lazy(() => import("./Pages/Projet"));
+const Cv = React.lazy(() => import("./Pages/Cv"));
+const Contact = React.lazy(() => import("./Pages/Contact"));
+// import Projet from  './Pages/Projet'
+// import Cv from  './Pages/Cv'
+// import Contact from  './Pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={HeadBodyGrid}>
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/projet"> Projet </Link>
+          <Link to="/cv"> CV </Link>
+          <Link to="/contact"> Contact </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projet" element={<Projet />} />
+          <Route path="/cv" element={<Cv />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound404 />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
---
+++ src/Pages/Projet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projet = () => {
+  return <div>Projet</div>;
+};
+
+export default Projet;
